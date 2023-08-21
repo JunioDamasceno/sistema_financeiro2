89eeafc99b7524cc31cebb4cbaf2900ea8df92a6
JunioDamasceno <juniowin@yahoo.com.br>
2023-08-20 22:13:24 -0300

    Versao original do programa

---
+++ README.md
@@ -0,0 +1,27 @@
+# **Sistema Bancário**
+Programa desenvolvido para atividade à ser entregue do curso "Potência Powered by Ifood | Ciênca de dados com Python" da DIO.
+
+Trata-se de um progrma simples que simula três operações de um sistema bancário:
+
+* Depósito;
+* Saque;
+* Extrato;
+
+Regras:
+
+Não pode haver saques ou depósitos com valor negativo
+Não é possível sacar valor maior que R$ 500,00 por operação
+Não é permitida mais que três operações de saque.
+
+O programa pede que o usuário escolha uma das operações: Depósito, saque, extrato ou sair do programa.
+
+Se escolhida a opção depósito, o programa pede que o usuário informe o valor do depósito. Caso o usuário entre com alguma informação inválida o programa deve retornar uma mensagem e retornar para a tela inicial da oparação.
+
+Se escolhida a opção depósito, o programa pede que o usuário informe o valor do saque. Caso o usuário entre com alguma informação inválida o programa deve retornar uma mensagem e retornar para a tela inicial da oparação.
+
+Se escolhida a opção extrato, o programa deve mostrar em sequẽncia todas as operações de depósito e saque que foram realizadas, e exibir no final o saldo da conta.
+
+Se escolhida a opção sair, o programa será fechado.
+
+
+
---
+++ sistema_bancario2.py
@@ -0,0 +1,88 @@
+#Programa desenvolvido para atividade do curso "Potência Poweded by Ifood | Ciência de dados" da DIO
+
+#Programa simples que simula três operações básicas de um sistema bancário: Depósito, saque e extrato
+#Regra 1: Não pode haver saques e depósitos em valor negativo
+#Regra 2: Não é possível sacar valor maior que R$ 500,00 por operação
+#Regra 3: Não é permitida mais que três operações de saque
+
+#para dar clear no terminal quando o sistema for fechado.
+from os import system
+#Para registrar data e hora no sistema quando for gravadas as operações de saque ou depósito.
+from datetime import datetime
+
+saldo = 0
+limval_saque = 500
+extrato = []
+i = 0
+j = 0
+nsaques = 0
+lim_saques = 3
+opcao = ''
+
+menu = """
+Escolha uma das opções abaixo:
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+"""
+
+while opcao != 'q':
+    print(menu)
+    opcao = input("\nDigite a opção desejada: ")
+
+    if opcao == 'd': 
+        valor = float(input("\nDigite o valor de depósito: "))
+        if valor > 0:
+            saldo = saldo + valor
+            extrato.insert(i, "{}  Depósito  R$ {:.2f}".format(datetime.now().strftime('%d/%m/%Y %H:%M'), valor))
+            i = i + 1
+            print("\n*** Deposito de R$ {:.2f} realizado com sucesso! ***\n".format(valor))
+            fechar = input("\nPressione qualquer tecla para voltar ao menu: ")
+            if fechar != None: 
+                system('clear')
+                fechar = None
+                fechar = ''
+
+        else: print("\n*** Valor não permitido. Só é aceito valores para depósito maiores que 0. ***")
+    
+    if opcao == 's':
+        if nsaques < lim_saques:
+            valor = float(input("\nDigite o valor para saque: "))
+            if valor > 0:
+                if valor > limval_saque:
+                    print("\n*** Valor de saque superior ao limite permitido, insira um valor menor que R$ 500,00 ***")
+                else:
+                    if valor >= saldo:
+                        print('\n*** Você não possui saldo suficiente para este saque! ***')
+                    else:
+                        saldo = saldo - valor
+                        extrato.insert(i, "{}  Saque     -R$ {:.2f}".format(datetime.now().strftime('%d/%m/%Y %H:%M'), valor))
+                        print("\n*** O Saque de R$ {:.2f} foi realizado com sucesso! ***".format(valor))
+                        nsaques = nsaques + 1
+                        fechar = input("\nPressione qualquer tecla para voltar ao menu: ")
+                        if fechar != None:
+                            system('clear')
+                            fechar = None
+                            fechar = ''
+            else: print("\n*** Valor não permitido. Só são aceitos valores para saque maiores que 0. ***")
+        else: print("\n***  quantidade de limites de saques por operação foi excedida! ***")
+    
+    if opcao == 'e':
+        for linha in extrato:
+            j = j + 1
+            print(j, linha, '\n')
+        j = 0
+        print("*** Saldo em {} R$ {:.2f} ***".format(datetime.now().strftime('%d/%m/%Y %H:%M') ,saldo))
+        fechar = input("\nPressione qualquer tecla para voltar ao menu: ")
+        if fechar != None:
+            system('clear')
+            fechar = None
+            fechar = ''
+
+system('clear')
+print("\nPrograma fechado com sucesso, muito obrigado por utilizar nossos serviços!")
+print('\nTenha um bom dia!\n')
+            
